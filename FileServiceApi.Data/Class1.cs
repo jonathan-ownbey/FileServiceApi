26bc3f0fd3daa7a7df605d3fae8b2268b039683c
Jon Ownbey <jonathan.ownbey@gmail.com>
2018-08-24 16:52:08 -0400

    Initial commit
    Projects added.

---
+++ FileServiceApi.Data/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace FileServiceApi.Data
+{
+    public class Class1
+    {
+    }
+}
---
+++ FileServiceApi.FileStorers/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace FileServiceApi.FileStorers
+{
+    public class Class1
+    {
+    }
+}
